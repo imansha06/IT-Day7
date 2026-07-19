3dd7defe79167f8aa6c890d050b0f00cbf4a9aee
Divya Imansha <imanshaaa06@gmail.com>
2026-07-19 15:09:26 +0530

    third commit

--- app.js/app.js
+++ app.js/app.js
@@ -1,91 +1,21 @@
-// // const customerList = [];
+fetch("https://jsonplaceholder.typicode.com/todos").then(res=>res.json()).then(data=>{
+    console.log(data);
 
-// // customerList.push(1);
-// // customerList.push(2);
-// // customerList.push(3);
+    let tblTodoList = document.getElementById("tblTodoList");
 
-// // let revAr = customerList.reverse();
+    let body=""
 
-// // console.log(revAr);
+    data.forEach(element => {
+        body+= `
+                <tr>
+            <td>${element.id}</td>
+            <td>${element.title}</td>
+            <td>${element.userId}</td>
+            <td>${element.completed}</td>
+        </tr>
+        `
+    });
 
+    tblTodoList.innerHTML=body;
 
-// const products = [
-//     { name: "bun", inStock: true },
-//     { name: "car", inStock: false },
-//     { name: "bat", inStock: true },
-//     { name: "van", inStock: false },
-// ];
-
-
-// let inStockItems = products.filter(
-//         function (product) {
-//             return productFlilter(prodcut)
-//         }
-// )
-
-// function productFlilter(product){
-//     return product.inStock == true;
-// }
-
-// console.log(inStockItems);
-
-
-
-// function getSum(num1,num2){
-//     return num1 + num2;
-// }
-
-// let getSum = function(num1,num2){
-//     return num1 + num2
-// }
-
-// console.log(getSum(10,20));
-
-
-// let getSum = (num1 + num2) => {
-//     return num1 +num2;
-// }
-
-// console.log(getSum(10,20));
-
-
-// let sample = txtValue =>{
-//     return txtValue;
-// }
-
-// console.log(sample("Hi Saman"));
-
-// let getSum = (num1,num2) => num1+num2;
-
-// console.log(getSum(10,20));
-
-
-// let sample = txtValue=> txtValue;
-
-// console.log(sample("Hi Saman"));
-
-
-
-// const employeSalary = [75000,25000,45000,55000];
-
-// let newSalaryArray = employeSalary.map(salary=>salary*2);
-
-// console.log(newSalaryArray);
-
-
-
-// const studentList = [
-//     {id:"001",name:"saman"},
-//     {id:"002",name:"kamal"},
-//     {id:"003",name:"wimal"},
-//     {id:"004",name:"ranil"}
-// ];
-
-// let student = studentList.find(student=>student.id=="003");
-
-// console.log(student);
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//////json = java script object notation 
+})
